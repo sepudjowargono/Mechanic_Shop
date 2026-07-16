1f0cadaae3bf70683ab74cf147be4f7c03513691
Stephana Pudjowargono <sepudjowargono@gmail.com>
2026-07-16 15:13:34 -0400

    Include swagger.yaml file in code and any changes made to it. This file contains the OpenAPI specification for the API endpoints, including request and response schemas, parameters, and examples. It is essential for generating API documentation and ensuring that the API adheres to the defined contract.

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,21 @@ from .models import db
 from .blueprints.customers import customers_bp
 from .blueprints.mechanics import mechanics_bp
 from .blueprints.service_tickets import service_tickets_bp
-from.blueprints.inventory import inventory_bp
+from .blueprints.inventory import inventory_bp
+from flask_swagger_ui import get_swaggerui_blueprint
 
+SWAGGER_URL = '/api/docs' # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.yaml' # Our API url (can of course be a local resource)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Mechanic Shop API"
+    }
+)
+
+# Call factory function to create the Flask app
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(f'config.{config_name}')
@@ -21,5 +34,6 @@ def create_app(config_name):
     app.register_blueprint(mechanics_bp, url_prefix='/mechanics')
     app.register_blueprint(service_tickets_bp, url_prefix='/service_tickets')
     app.register_blueprint(inventory_bp, url_prefix='/inventory')
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL) # Register Swagger UI blueprint
     
     return app
